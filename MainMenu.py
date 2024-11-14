2b1f6fd94e34ec0be01f729b21d010dc52cb6f9b
Corbin McCalister <mccalisterc2003@gmail.com>
2024-11-14 12:00:59 -0600

    New Character Page Added

--- MainMenu.py
+++ MainMenu.py
@@ -1,7 +1,9 @@
 
 # libraries Import
 from tkinter import *
+import tkinter as tk
 import customtkinter
+import NewCharacter
 
 # Main Window Properties
 
@@ -10,9 +12,25 @@ window.title("Tkinter")
 window.geometry("1280x720")
 window.configure(bg="#FFFFFF")
 
+frame = Frame(window, width="1280", height="720")
+frame.pack(expand=True, fill="both")
 
+Label_id2 = customtkinter.CTkLabel(
+    master=frame,
+    text="Main Menu",
+    font=("Arial", 30),
+    text_color="#000000",
+    height=30,
+    width=200,
+    corner_radius=0,
+    bg_color="#FFFFFF",
+    fg_color="#FFFFFF",
+    compound="center",
+    anchor="center"
+    )
+Label_id2.pack(side=tk.TOP, expand=True)
 Button_id4 = customtkinter.CTkButton(
-    master=window,
+    master=frame,
     text="Play Game",
     font=("undefined", 16),
     text_color="#000000",
@@ -27,8 +45,9 @@ Button_id4 = customtkinter.CTkButton(
     fg_color="#00ff55",
     )
 Button_id4.place(x=600, y=200)
+Button_id4.pack(side=tk.TOP, expand=True)
 Button_id1 = customtkinter.CTkButton(
-    master=window,
+    master=frame,
     text="New Character",
     font=("undefined", 16),
     text_color="#000000",
@@ -41,10 +60,12 @@ Button_id1 = customtkinter.CTkButton(
     border_color="#000000",
     bg_color="#FFFFFF",
     fg_color="#F0F0F0",
+    command=lambda: NewCharacter.updateWindow(window, frame)
     )
 Button_id1.place(x=600, y=250)
+Button_id1.pack(side=tk.TOP, expand=True)
 Button_id3 = customtkinter.CTkButton(
-    master=window,
+    master=frame,
     text="Edit Character",
     font=("undefined", 16),
     text_color="#000000",
@@ -59,8 +80,9 @@ Button_id3 = customtkinter.CTkButton(
     fg_color="#F0F0F0",
     )
 Button_id3.place(x=600, y=300)
+Button_id3.pack(side=tk.TOP, expand=True)
 Button_id5 = customtkinter.CTkButton(
-    master=window,
+    master=frame,
     text="Remove Character",
     font=("undefined", 16),
     text_color="#000000",
@@ -75,36 +97,9 @@ Button_id5 = customtkinter.CTkButton(
     fg_color="#F0F0F0",
     )
 Button_id5.place(x=600, y=350)
-Button_id7 = customtkinter.CTkButton(
-    master=window,
-    text="Exit Game",
-    font=("undefined", 16),
-    text_color="#000000",
-    hover=True,
-    hover_color="#9a1d1d",
-    height=30,
-    width=150,
-    border_width=2,
-    corner_radius=6,
-    border_color="#000000",
-    bg_color="#FFFFFF",
-    fg_color="#ff0000",
-    )
-Button_id7.place(x=600, y=450)
-Label_id2 = customtkinter.CTkLabel(
-    master=window,
-    text="Main Menu",
-    font=("Arial", 30),
-    text_color="#000000",
-    height=30,
-    width=200,
-    corner_radius=0,
-    bg_color="#FFFFFF",
-    fg_color="#FFFFFF",
-    )
-Label_id2.place(x=580, y=150)
+Button_id5.pack(side=tk.TOP, expand=True)
 Button_id6 = customtkinter.CTkButton(
-    master=window,
+    master=frame,
     text="Character Details",
     font=("undefined", 16),
     text_color="#000000",
@@ -119,6 +114,24 @@ Button_id6 = customtkinter.CTkButton(
     fg_color="#F0F0F0",
     )
 Button_id6.place(x=600, y=400)
+Button_id6.pack(side=tk.TOP, expand=True)
+Button_id7 = customtkinter.CTkButton(
+    master=frame,
+    text="Exit Game",
+    font=("undefined", 16),
+    text_color="#000000",
+    hover=True,
+    hover_color="#9a1d1d",
+    height=30,
+    width=150,
+    border_width=2,
+    corner_radius=6,
+    border_color="#000000",
+    bg_color="#FFFFFF",
+    fg_color="#ff0000",
+    )
+Button_id7.place(x=600, y=450)
+Button_id7.pack(side=tk.TOP, expand=True)
 
 
 
